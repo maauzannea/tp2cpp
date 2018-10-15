f6b7797dcf76f7191407485157c0ae78e2a4f581
maauzannea <maauzannea@etud.local.isima.fr>
2018-10-15 14:06:44 +0200

    Test 1 OK

--- build/CMakeFiles/tp2_test.dir/depend.make
+++ build/CMakeFiles/tp2_test.dir/depend.make
@@ -17,6 +17,10 @@ CMakeFiles/tp2_test.dir/src/polaire.cpp.o: ../src/polaire.hpp
 CMakeFiles/tp2_test.dir/test/catch.cpp.o: ../test/catch.cpp
 CMakeFiles/tp2_test.dir/test/catch.cpp.o: ../test/catch.hpp
 
+CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../src/cartesien.hpp
+CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../src/nuage.hpp
+CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../src/point.hpp
+CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../src/polaire.hpp
 CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../test/catch.hpp
 CMakeFiles/tp2_test.dir/test/tp2_test.cpp.o: ../test/tp2_test.cpp
 
--- src/nuage.hpp
+++ src/nuage.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "point.hpp"
+#include "cartesien.hpp"
+#include "polaire.hpp"
 #include <vector>
 
 template <typename T> class Nuage {
@@ -9,20 +11,35 @@ template <typename T> class Nuage {
 	public:
 		Nuage();
 		~Nuage();
-		void ajouter(T&);
+		void ajouter(T);
 		size_t size() const;
-		typedef std::vector<T*>::const_iterator const_iterator;
+		typedef typename std::vector<T*>::const_iterator const_iterator;
 		const_iterator begin() const;
 		const_iterator end() const;
 };
 
 template <typename T>
-Nuage::Nuage() {}
+Nuage<T>::Nuage() {}
 
 template <typename T>
-Nuage::~Nuage() {}
+Nuage<T>::~Nuage() {}
 
 template <typename T>
-void Nuage::ajouter(T &p) {
-	
+void Nuage<T>::ajouter(T p) {
+	vectPoints.push_back(&p);
+}
+
+template <typename T>
+size_t Nuage<T>::size() const {
+	return vectPoints.size();
+}
+
+template <typename T>
+typename Nuage<T>::const_iterator Nuage<T>::begin() const {
+	return vectPoints.begin();
+}
+
+template <typename T>
+typename Nuage<T>::const_iterator Nuage<T>::end() const {
+	return vectPoints.end();
 }
